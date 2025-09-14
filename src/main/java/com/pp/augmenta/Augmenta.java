1825f04d6921c7e96b66e64811661bac86ea4a7a
Phlompf <monsterblue8991@gmail.com>
2025-09-14 19:47:01 -0400

    yo mom

---
+++ src/main/java/com/pp/augmenta/Augmenta.java
@@ -0,0 +1,97 @@
+package com.pp.augmenta;
+
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.block.SoundType;
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+@Mod(Augmenta.MODID)
+public class Augmenta {
+    public static final String MODID = "augmenta";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    // Block Register
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
+    public static final DeferredRegister.Items  ITEMS  = DeferredRegister.createItems(MODID);
+
+    // Creative Tab Registry
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+
+    // Blocks (and their items)
+    public static final DeferredBlock<Block> AUGMENTER = BLOCKS.registerSimpleBlock("augmenter", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
+    public static final DeferredItem<BlockItem> AUGMENTER_ITEM = ITEMS.registerSimpleBlockItem("augmenter", AUGMENTER);
+
+    // Register items in the creative tab
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("augmenta", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.augmenta"))
+            .withTabsBefore(CreativeModeTabs.COMBAT)
+            .icon(() -> AUGMENTER_ITEM.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                output.accept(AUGMENTER_ITEM.get());
+            }).build());
+
+    // First code that is run when mod is loaded.
+    public Augmenta(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+
+        // Finalize Registries
+        BLOCKS.register(modEventBus);
+        ITEMS.register(modEventBus);
+        CREATIVE_MODE_TABS.register(modEventBus);
+
+        // Register ourselves for server and other game events we are interested in.
+        NeoForge.EVENT_BUS.register(this);
+        modEventBus.addListener(this::addCreative);
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+        LOGGER.info("HELLO FROM COMMON SETUP");
+
+        if (Config.LOG_DIRT_BLOCK.getAsBoolean()) {
+            LOGGER.info("DIRT BLOCK >> {}", BuiltInRegistries.BLOCK.getKey(Blocks.DIRT));
+        }
+    }
+
+    // Add items to the creative tab group
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(AUGMENTER_ITEM);
+        }
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        // Do something when the server starts
+        LOGGER.info("HELLO from Server Starting.");
+    }
+}
